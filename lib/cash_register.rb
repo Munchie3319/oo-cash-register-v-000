9ed12c2ff6984560386fe8edcfe01fe5e4bcc258
rosa lee <rosalee0116@gmail.com>
2020-04-08 20:11:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,4 +7,10 @@ class CashRegister
     @discount = discount
     @items = []
   end
+
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do 
+      items << title
+    end
 end
